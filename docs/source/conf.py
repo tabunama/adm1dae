a83e10a52de254ac967b0fc3c6db24b0f080465f
tabunama <tabunama@gmail.com>
2026-04-03 21:01:38 +0200

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from pathlib import Path
 
@@ -7,7 +8,6 @@ sys.path.insert(0, str(SRC))
 
 project = "adm1dae"
 author = "Taher Abunama"
-copyright = "2026"
 release = "0.1.0"
 
 extensions = [
@@ -16,7 +16,14 @@ extensions = [
     "sphinx.ext.viewcode",
 ]
 
+autodoc_mock_imports = [
+    "adm1dae.adm1_DAE2_bsm2",
+    "adm1dae.pHsolv_bsm2",
+    "adm1dae.Sh2solv_adm1",
+]
+
 templates_path = ["_templates"]
 exclude_patterns = []
+
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
